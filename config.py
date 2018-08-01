ef20b54c370e246eebb966e65083bb1cd9d40417
Nader Abolfazli <abolfazli.nader@gmail.com>
2018-08-01 10:02:24 +0430

    adding balebot and config

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+import logging
+
+
+class BotConfig:
+    base_url = os.environ.get('BASE_URL', None) or "wss://api.bale.ai/v1/bots/"
+    bot_token = os.environ.get('TOKEN', None) or "ab876eea51b5fd8121e2c20f8baed500c260add5"
+    system_local = os.environ.get('SYSTEM_LOCAL', None) or "fa_IR"
+    resending_max_try = int(os.environ.get('RESENDING_MAX_TRY', 5))
+    reuploading_max_try = int(os.environ.get('REUPLOADING_MAX_TRY', 5))
+
+
+class DbConfig:
+    db_user = os.environ.get('POSTGRES_USER', None)
+    db_password = os.environ.get('POSTGRES_PASSWORD', None)
+    db_host = os.environ.get('POSTGRES_HOST', None)
+    db_name = os.environ.get('POSTGRES_DB', None)
+    db_port = os.environ.get('POSTGRES_PORT', None)
+    # database_url = "postgresql://{}:{}@{}:{}/{}".format(db_user, db_password, db_host, db_port, db_name) or None
+    database_url = "postgresql://postgres:nader1993@localhost:5432/testdb"
